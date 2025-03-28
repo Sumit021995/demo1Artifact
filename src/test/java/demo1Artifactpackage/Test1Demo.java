d8dbf17f79a84a741c53163f01e9828471aebbc9
Sumit021995 <kaku.saurav@gmail.com>
2025-03-28 10:23:18 +0530

    commit4

--- src/test/java/demo1Artifactpackage/Test1Demo.java
+++ src/test/java/demo1Artifactpackage/Test1Demo.java
@@ -11,5 +11,6 @@ public class Test1Demo {
 		System.out.println("Hiiiii from Test Lead Framework");
 		System.out.println("Hiiiii from Sumit");
 		System.out.println("After pulling from master other commit");
+		System.out.println("After pulling from master other commit2");
 	}
 }
