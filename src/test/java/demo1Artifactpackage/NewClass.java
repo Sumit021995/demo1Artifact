413362bbe13083aab29804703a85e4ad1062d680
Sumit021995 <kaku.saurav@gmail.com>
2025-03-29 09:24:32 +0530

    new commit from NewClass

---
+++ src/test/java/demo1Artifactpackage/NewClass.java
@@ -0,0 +1,10 @@
+package demo1Artifactpackage;
+
+import org.testng.annotations.Test;
+
+public class NewClass {
+	@Test
+	public static void newMethod() {
+		System.out.println("from new class");
+	}
+}
